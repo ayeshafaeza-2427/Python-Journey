a3adb6db13668a48d868aa22a8c38acb2fa868c8
Ayesha Faeza <ayeshafaeza2427@gmail.com>
2026-05-24 20:45:57 +0530

    Create data_types.py

---
+++ 2. Data Types/data_types.py
@@ -0,0 +1,53 @@
+# Integer Data Type
+age = 18
+marks = 95
+
+print("Integer Examples:")
+print(age)
+print(marks)
+
+# Float Data Type
+height = 5.7
+price = 99.99
+
+print("\nFloat Examples:")
+print(height)
+print(price)
+
+# String Data Type
+name = "Ayesha"
+city = "Hyderabad"
+
+print("\nString Examples:")
+print(name)
+print(city)
+
+# Boolean Data Type
+is_student = True
+is_logged_in = False
+
+print("\nBoolean Examples:")
+print(is_student)
+print(is_logged_in)
+
+# Type Checking
+print("\nType Checking:")
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+# Type Conversion
+print("\nType Conversion:")
+
+string_number = "25"
+converted_number = int(string_number)
+
+print(converted_number)
+print(type(converted_number))
+
+number = 100
+converted_string = str(number)
+
+print(converted_string)
+print(type(converted_string))

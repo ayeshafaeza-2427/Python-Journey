65b1150548ae8268dbdd69c0bbf674dd987bb057
Ayesha Faeza <ayeshafaeza2427@gmail.com>
2026-05-29 11:12:56 +0530

    Create comparision operators.py

---
+++ 3. .Operators/comparision operators.py
@@ -0,0 +1,10 @@
+print("===== Comparison Operators =====")
+
+print("Equal to:", a == b)
+print("Not Equal to:", a != b)
+print("Greater than:", a > b)
+print("Less than:", a < b)
+print("Greater than or Equal to:", a >= b)
+print("Less than or Equal to:", a <= b)
+
+print("\n")

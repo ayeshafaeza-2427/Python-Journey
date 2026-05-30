7eec4174a21d8c7d0582d272ce953d28282e8c7b
Ayesha Faeza <ayeshafaeza2427@gmail.com>
2026-05-30 14:49:53 +0530

    Create assignment_operators.py

---
+++ 3. .Operators/assignment_operators.py
@@ -0,0 +1,19 @@
+num = 10
+
+print("===== Assignment Operators =====")
+
+print("Initial Value:", num)
+
+num += 5
+print("After += :", num)
+
+num -= 3
+print("After -= :", num)
+
+num *= 2
+print("After *= :", num)
+
+num /= 4
+print("After /= :", num)
+
+print("\n")

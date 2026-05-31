e2f184a501c8137c66c862bd8d37058b6265c28e
Ayesha Faeza <ayeshafaeza2427@gmail.com>
2026-05-31 11:21:09 +0530

    Create nested_if.py

---
+++ 4. Conditonal Statements/nested_if.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    has_license = input("Do you have a driving license? (yes/no): ")
+
+    if has_license.lower() == "yes":
+        print("You can drive.")
+    else:
+        print("You cannot drive without a license.")
+else:
+    print("You are not old enough to drive.")

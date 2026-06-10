7244dd331b9ec98fd74728fd42ff185415464059
Ayesha Faeza <ayeshafaeza2427@gmail.com>
2026-06-10 16:26:57 +0530

    Create 4. nested_if.py

--- 4. Conditonal Statements/4. nested_if.py
+++ 4. Conditonal Statements/4. nested_if.py
@@ -6,6 +6,6 @@ if age >= 18:
     if has_license.lower() == "yes":
         print("You can drive.")
     else:
-        print("You cannot drive without a license.")
+        print("You can't drive without a license.")
 else:
     print("You are not old enough to drive.")
